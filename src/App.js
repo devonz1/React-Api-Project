d5b3ac2221c70aa2e1090075a9dcf643997f9b37
devon gibson <devongibs2@gmail.com>
2023-03-01 23:39:00 -0800

    updates

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+ import shapef from "C:/Users/devon/Promineo/react-api-project/src/components/shapef.png";  
+import Create from './components/create';
+import './App.css';
+
+function App() {
+  return (
+       <> 
+    <div className='main'>
+ <img src={shapef} alt="logo"/> 
+   <div>
+      <Create/>
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
